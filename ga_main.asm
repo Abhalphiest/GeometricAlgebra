94534b9d0c7962a0204d441a54328482ecf9e1c5
Margaret <med7068@rit.edu>
2016-03-24 00:07:39 -0400

    input is implemented, and seems to work without error. Formatting leaves something to be desired, and we still need input validation and the object building function to be able to test our second print function, but making good progress.

--- ga_main.asm
+++ ga_main.asm
@@ -39,6 +39,9 @@ product_label:
 newline:
 	.asciiz "\n"
 
+	.align 	2
+input_arr:
+	.space 32	#the max we would need for 3 dim
 #
 # FUNCTIONS
 #
@@ -52,20 +55,30 @@ newline:
 #	.globl	geom_product
 
 main:
-	addi	$sp, $sp, -24		#build our stack frame
+	addi	$sp, $sp, -28		#build our stack frame
 	sw	$ra, 0($sp)
 	sw	$s0, 4($sp)
 	sw	$s1, 8($sp)
 	sw	$s2, 12($sp)
 	sw	$s3, 16($sp)
 	sw	$s4, 20($sp)
+	sw	$s5, 24($sp)
 
 	or	$s4, $zero, $zero	#loop control
 	ori	$s3, $zero, DIMENSION	#set up for loop bound
+	la	$s5, input_arr
 get_input_loop:
 	slt	$t0, $s4, $s3		#will be 1 if we are still in
 	beq	$t0, $zero, validate_input
-
+	or	$a0, $zero, $s4
+	jal	print_basis
+	ori	$v0, $zero, READ_INT
+	syscall
+	sw	$v0, 0($s5)
+	addi	$s5, $s5, 4
+	addi	$s4, $s4, 1
+	j	get_input_loop
+	
 validate_input:
 
 	lw	$ra, 0($sp)
@@ -74,5 +87,6 @@ validate_input:
 	lw	$s2, 12($sp)
 	lw	$s3, 16($sp)
 	lw	$s4, 20($sp)
-	addi	$sp, $sp, 24
+	lw	$s5, 24($sp)
+	addi	$sp, $sp, 28
 	jr	$ra		#return
